bc92556c389cc0ab97300a34a89f4c0bdd527063
memoji254 <memojidevinyo@gmail.com>
2024-10-30 14:38:15 +0300

    Update config.js

--- config.js
+++ config.js
@@ -4,8 +4,8 @@ const fs = require('fs')
 const { color } = require('./lib/myfunc')
 
 //owner
-global.owner = '2347043759577'
-global.nomerowner = ["2347043759577"]
+global.owner = '254700639518'
+global.nomerowner = ["254700639518"]
 
 // Apikey 
 
